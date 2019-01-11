bd0489b109d37e5e094971ca133be033e2198687
Corinna Koch <cokoch@uni-bremen.de>
2019-01-11 14:50:57 +0100

    removed move-to-standard-poses.lisp (this will be handled with giskard)

--- planning_communication/src/move-to-standard-poses.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :pc)
-
-(defun move-to-standard-pose (poseName)
-  "moves to standard pose"
-  (with-ros-node ("move_to_standard_pose")
-    (if (not (wait-for-service "standard_poses" 10))
-      (ros-warn nil "Timed out waiting for service standard_poses")
-      (format t "~a"(call-service "standard_poses" 'standard_poses-srv:StandardPoses :poseName poseName)))))
-
