3eed8dc9ce7ac5f8740210e6b41fb1c3ebcc8983
Alina Hawkin <hawkin@informatik.uni-bremen.de>
2019-01-18 16:26:38 +0100

    [plc] added init function which launches all the action servers

--- planning_common/src/init.lisp
+++ planning_common/src/init.lisp
@@ -1,1 +1,12 @@
 (in-package :plc)
+
+(defun init-planning ()
+  "Initialize all the interfaces from planning to other groups."
+
+  (roslisp-utilities:startup-ros :name "planning-node" :anonymous nil)
+
+  ;;Init all the action servers
+  (pc:init-action-client) ;; for text-to-speech
+  (chll:init-nav-client)
+  (roslisp:ros-info (plc) "All action server clients are set up.")) ;; for navigation
+  
