fbf350b2335cf14293269e57193c77465defb4ba
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2019-04-24 22:02:36 +0200

    Make head client and tf-listener safer. Add rosnode executable function

--- planning_common/src/utils.lisp
+++ planning_common/src/utils.lisp
@@ -8,9 +8,10 @@
   (unless *tf-listener*
     (setf *tf-listener* (make-instance 'cl-tf2:buffer-client))
     (handler-case
-     (cl-tf2:lookup-transform *tf-listener* "map" "odom" :timeout 3)
-     (CL-TRANSFORMS-STAMPED:TIMEOUT-ERROR
-      () (roslisp:ros-warn (get-tf-listener) "tf-listener takes longer than 3 seconds to get odom in map."))))
+     (cl-tf2:lookup-transform *tf-listener* "map" "odom" :timeout 20)
+      (CL-TRANSFORMS-STAMPED:TRANSFORM-STAMPED-ERROR () (roslisp:ros-warn (get-tf-listener) "tf-listener takes longer than 20 seconds to get odom in map."))
+      (CL-TRANSFORMS-STAMPED:TIMEOUT-ERROR
+       () (roslisp:ros-warn (get-tf-listener) "tf-listener takes longer than 20 seconds to get odom in map."))))
   *tf-listener*)
 
 (defun kill-tf-listener ()
--- planning_execute_demo/src/package.lisp
+++ planning_execute_demo/src/package.lisp
@@ -1,3 +1,9 @@
 (defpackage :planning-execute-demo
   (:nicknames :pexe)
-  (:use :roslisp :cl))
+  (:use :roslisp :cl)
+  (:export
+   ;; main
+   #:main
+   #:michels-grasping-routine-with-perception-and-knowledge-for-testing-which-objects-work-out-booooy-how-long-can-a-function-name-get-oh-my-lord-is-this-huge
+   
+   ))
