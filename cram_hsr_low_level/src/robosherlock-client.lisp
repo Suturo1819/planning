079fc1c8f5d0efae0f80dcc95d94675f8a25f310
vanessa-rin <hassouna@uni-bremen.de>
2019-04-25 20:31:20 +0200

    [chll] door opening is now working with vizbox

--- cram_hsr_low_level/src/robosherlock-client.lisp
+++ cram_hsr_low_level/src/robosherlock-client.lisp
@@ -44,14 +44,14 @@
 
 (defun init-robosherlock-door-action-client ()
   (roslisp:ros-info (robosherlock-client)
-                    "Creating robosherlock action client for server 'extract_object_infos'.")
+                    "Creating robosherlock action client for server 'analye_shelf_status'.")
   (setf *robosherlock-action-client*
         (actionlib:make-action-client "/analyze_shelf_status"
                                       "suturo_perception_msgs/AnalyzeShelfStatusAction"))
   (loop until (actionlib:wait-for-server *robosherlock-action-client*
                                          *robosherlock-action-timeout*))
   (roslisp:ros-info (robosherlock-client)
-                    "Robosherlock door action client for ~a created." "'extract_object_infos'"))
+                    "Robosherlock door action client for ~a created." "'analye_shelf_status'"))
 
 (defun get-robosherlock-door-client ()
   (unless *robosherlock-action-client*
@@ -59,13 +59,20 @@
   *robosherlock-action-client*)
 
 (defun call-robosherlock-door-pipeline ()
+  "returns true=open or nil=false"
+  (pc:publish-operator-text "Toya is the door open?")
   (roslisp:ros-info (robosherlock-client) "Calling pipeline for door.")
   ;; actual call
-  (actionlib:call-goal (chll::get-robosherlock-door-client)
+  (roslisp:with-fields (door_open)
+      (actionlib:call-goal (chll::get-robosherlock-door-client)
                        (roslisp:make-message
                         "suturo_perception_msgs/AnalyzeShelfStatusActionGoal")
                        :timeout *robosherlock-action-timeout*
-                       :result-timeout *robosherlock-action-timeout*))
+                       :result-timeout *robosherlock-action-timeout*)
+    (if door_open
+    (pc:publish-robot-text "The door is open Operator we can continute.")
+    (pc:publish-robot-text "The door is shut Operator i will wait."))
+    door_open))
 
 
 
